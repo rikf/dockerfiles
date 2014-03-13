776c7c2f50bc5ac1982392b6d8fdf2ab5c41c93c
Richard Friend <rik.friend@gmail.com>
2014-03-13 15:52:02 +1100

    Some extra logging added and using a more fine grained aggregation strategy

--- graphite/run.sh
+++ graphite/run.sh
@@ -5,11 +5,17 @@ if [ ! -z $SECRET_KEY ]; then
 fi
 
 mkdir -p /opt/graphite/storage/log/webapp
+mkdir -o /opt/graphite/storage/log/carbon-cache/carbon-cache-a/
 mkdir -p /opt/graphite/storage/whisper
 touch /opt/graphite/storage/graphite.db /opt/graphite/storage/index
 chown -R www-data /opt/graphite/storage
 chmod 0775 /opt/graphite/storage /opt/graphite/storage/whisper
 chmod 0664 /opt/graphite/storage/graphite.db
 cd /opt/graphite/webapp/graphite && python manage.py syncdb --noinput
+
+echo "================"
+ls -allh /opt/graphite/conf
+cat /opt/graphite/conf/storage-schemas.conf
+echo "================"
 
 /usr/bin/supervisord
